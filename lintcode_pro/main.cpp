676f50a6133aa1628ef7c98717b15f7dbc6633fc
liuyan <ly2010jlu13@163.com>
2020-08-31 11:40:06 +0800

    find substr from source string, use KMP mothod
    Signed-off-by: liuyan <ly2010jlu13@163.com>

--- lintcode_pro/main.cpp
+++ lintcode_pro/main.cpp
@@ -7,15 +7,21 @@
 //#include "source37.h"
 //#include "source463.h"
 //#include "source2.h"
-#include "source8.h"
-
+//#include "source8.h"
+#include "source13.h"
 
 using namespace std;
 
 
 int main() {
     
-    Solution8 sol;
+    Solution13 sol;
+
+    string source = "ABABCDDBCA";
+    string target = "DDB";
+
+    int res = sol.strStr(source, target);
+
 
     return 0;
 }
